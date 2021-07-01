f1782a81b477fe154f3b3d240ea4d2f6cdb47e5b
Rachel Courtney <71882629+rachelcourtney@users.noreply.github.com>
2021-07-01 01:35:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="Dublin_Bus",
+      version="0.1",
+      description="Dublin Bus Web Application",
+      url="",
+      author=["Rachel Courtney, Andrew McClean, Amanda Hegarty"],
+      author_email="",
+      licence="GPL3",
+      packages=['config'],
+
+      )
