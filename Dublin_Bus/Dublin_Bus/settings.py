e2a3ba41ed20be2e8f117f3b70a60fdde72eaa47
Rachel Courtney <rachel.courtney@ucdconnect.ie>
2021-08-06 23:25:28 +0100

    Database hidden key trial

--- Dublin_Bus/Dublin_Bus/settings.py
+++ Dublin_Bus/Dublin_Bus/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j04ekjnhy+s2+%&9vq$j88kvt4h+w6a_clxn3vde1&slb7co=#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'Dublin_Bus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'dublinbus',
-        'PASSWORD': 'the3amigos',
-        'HOST': 'dublinbus.casbkjuzj0uj.eu-west-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': env('ENGINE'),
+        'NAME': env('DATABASE'),
+        'USER': env('USERNAME'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
