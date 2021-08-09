0e160539abd8033f4f8aaf1986a6cc9f480d2df2
Andrew <andrewmcclean97@gmail.com>
2021-08-09 19:39:55 +0100

    bootstrappified Fare Calculator

--- Dublin_Bus/Bus/static/Bus/index.js
+++ Dublin_Bus/Bus/static/Bus/index.js
@@ -972,12 +972,15 @@ function resetJourneyPlanner() {
     fare_suggestions.style.display = "none";
 }
 
+
 function displayFareButtons() {
     var fare_suggestions = document.getElementById('fare_suggestions');
     if (fare_suggestions.style.display == "none") {
-    fare_suggestions.style.display = "block";
-    } else if (fare_suggestions.style.display == "block") {
-    fare_suggestions.style.display = "none";
+        $('#fareCalculator').attr('class', 'btn btn-info');
+        fare_suggestions.style.display = "block";
+    } else {
+        fare_suggestions.style.display = "none";
+        $('#fareCalculator').attr('class', 'btn btn-secondary');
     }
 }
 
@@ -1070,28 +1073,8 @@ $('#stops-tab-btn').on('shown.bs.tab', function() {
     checkFavourite();
 })
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//Trigger all popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
